b9d3eb374537e60812afa51f2cfe032ad1f94b25
Ikiseh Samuel <ikisehtochukwu@gmail.com>
2025-08-30 23:07:33 +0100

    Initial commit - My University Library System

---
+++ src/main/java/com/ikiseh/university_library_system/UniversityLibrarySystemApplication.java
@@ -0,0 +1,13 @@
+package com.ikiseh.university_library_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityLibrarySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityLibrarySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ikiseh/university_library_system/UniversityLibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikiseh.university_library_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityLibrarySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
